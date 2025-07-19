abe89eb12ba8c451b3878cc39ad15f64f9b6230c
Adhinov <133613543+adhinov@users.noreply.github.com>
2025-07-19 21:21:38 +0700

    db.js

--- db.js
+++ db.js
@@ -1,16 +1,13 @@
-const mysql = require("mysql2/promise");
-const dotenv = require("dotenv");
+const mysql = require("mysql2");
 
-dotenv.config();
-
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
 });
 
-module.exports = pool;
+module.exports = db;
